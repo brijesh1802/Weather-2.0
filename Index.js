ddb500da8cbdd1f6b66b4c44f8e4cf372f26a5fe
Hydra <brijeshpujari333@gmail.com>
2023-12-14 07:58:12 +0530

    Removed event listener in js file and the function calls on onclick attribute of button. Added comments to enhance readability.

--- Index.js
+++ Index.js
@@ -1,27 +1,30 @@
-
 async function weather() {
     try {
         document.querySelector(".error").style.display = "none";
-            document.querySelector(".main").style.display = "none";
+        document.querySelector(".main").style.display = "none";
         let city = document.querySelector(".city").value;
         let url = `http://api.weatherstack.com/current?access_key=364d6d51203295e9507c57d800116804&query=${city}`;
+        // Make an API call to get the weather data
         let response = await fetch(url);
         let data = await response.json();
         console.log(data);
 
+        // Check if the API call was successful
         if (!response.ok || !data.location) {
             document.querySelector(".error").style.display = "flex";
             document.querySelector(".main").style.display = "none";
         } else {
-            // Update styles and values
+             // If successful, update the HTML file with the weather data
             document.querySelector(".main").style.display = "flex";
             document.querySelector(".error").style.display = "none";
 
             let img = document.querySelector(".weatheri");
             document.querySelector(".cityn").innerHTML = data.location["name"];
             document.querySelector(".temp").innerHTML = data.current.temperature;
-            document.querySelector(".weather").innerHTML = data.current.weather_descriptions;
-            document.querySelector(".weatheri").innerHTML = data.current.weather_icons;
+            document.querySelector(".weather").innerHTML =
+                data.current.weather_descriptions;
+            document.querySelector(".weatheri").innerHTML =
+                data.current.weather_icons;
             document.querySelector(".windinfo").innerHTML = data.current.wind_speed;
             document.querySelector(".huminfo").innerHTML = data.current.humidity;
             document.querySelector(".prepinfo").innerHTML = data.current.precip;
@@ -30,9 +33,9 @@ async function weather() {
             if (wd == "Sunny") {
                 img.src = "./Images/sunny.png";
             } else if (wd == "Light Rain Possible") {
-                img.src = "./Images/lghtrain.png"
+                img.src = "./Images/lghtrain.png";
             } else if (wd == "Partly Cloudy") {
-                img.src = "./Images/partly_cloudy.png"
+                img.src = "./Images/partly_cloudy.png";
             } else if (wd == "Light Snow") {
                 img.src = "/Images/lightsnow.png";
             } else {
@@ -43,10 +46,3 @@ async function weather() {
         console.error("Error fetching weather data:", error);
     }
 }
-
-const searchbtn = document.querySelector(".searchbtn");
-
-searchbtn.addEventListener("click", () => {
-    weather();
-});
-
